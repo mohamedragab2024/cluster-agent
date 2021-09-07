075e2824cf88db25fef94b106d276a8273c04194
Mohamed Ragab <mohamed.ragab.ext@aptar.com>
2021-09-07 22:12:49 +0200

    add ingress logic

--- controllers/ingress.go
+++ controllers/ingress.go
@@ -9,6 +9,7 @@ import (
 	utils "github.com/kube-carbonara/cluster-agent/utils"
 	"github.com/labstack/echo/v4"
 	"k8s.io/api/extensions/v1beta1"
+	"k8s.io/client-go/1.5/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
@@ -33,12 +34,20 @@ func (c IngressController) Create(context echo.Context, nameSpaceName string, in
 		})
 	}
 
-	result := clientset.RESTClient().Post().
+	var result runtime.Object
+	createErr := clientset.RESTClient().Post().
 		NamespaceIfScoped(nameSpaceName, true).
 		Resource("ingress").
 		Body(ingress).
-		Do(ctx.TODO())
+		Do(ctx.TODO()).
+		Into(result)
 
+	if createErr != nil {
+		return context.JSON(http.StatusInternalServerError, models.Response{
+			Message:      createErr.Error(),
+			ResourceType: utils.RESOUCETYPE_INGRESS,
+		})
+	}
 	return context.JSON(http.StatusOK, models.Response{
 		Data:         utils.StructToMap(result),
 		ResourceType: utils.RESOUCETYPE_INGRESS,
