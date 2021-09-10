b359056992735fd0aba2b30f560b9bcbe18255f4
Mohamed Ragab <mohamed.ragab.ext@aptar.com>
2021-09-10 06:48:49 +0200

    refactor main

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func main() {
 		os.Exit(3)
 	}
 
-	session.Serv(40 * time.Second)
+	session.Serv(5 * time.Minute)
 
 	defer session.Conn.Close()
 	controllers.ServicesController{}.Watch(session.Conn)
