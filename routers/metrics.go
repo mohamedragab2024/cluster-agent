eef940d3b773c1769b39f3669c4c968a61204686
Mohamed Ragab <mohamed.ragab.ext@aptar.com>
2021-09-17 14:54:43 +0200

    add metrics

--- routers/metrics.go
+++ routers/metrics.go
@@ -1,11 +1,7 @@
 package routers
 
 import (
-	"net/http"
-
 	controllers "github.com/kube-carbonara/cluster-agent/controllers"
-	"github.com/kube-carbonara/cluster-agent/models"
-	"github.com/kube-carbonara/cluster-agent/utils"
 	"github.com/labstack/echo/v4"
 )
 
@@ -13,24 +9,8 @@ type MetricsRouter struct{}
 
 func (router MetricsRouter) Handle(e *echo.Echo) {
 	metricsController := controllers.MetricsController{}
-
-	e.GET("/metrics/:resource", func(context echo.Context) error {
-		switch context.Param("resource") {
-		case utils.RESOUCETYPE_NODES:
-			{
-				return metricsController.NodeMetrics(context)
-
-			}
-		default:
-			{
-				return context.JSON(http.StatusNotFound, models.Response{
-					Message: "Metrics not avaliable for this resource",
-				})
-
-			}
-
-		}
-
+	e.GET("/nodemetrics", func(context echo.Context) error {
+		return metricsController.NodeMetrics(context)
 	})
 
 }
