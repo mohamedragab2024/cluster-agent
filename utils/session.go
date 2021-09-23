f93248788ccf160867d3113f3092805323b0b3f4
Mohamed Ragab <mohamed.ragab.ext@aptar.com>
2021-09-23 19:09:32 +0200

    pass query string

--- utils/session.go
+++ utils/session.go
@@ -1,12 +1,10 @@
 package utils
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/url"
 	"os"
-	"os/signal"
 	"sync"
 
 	"github.com/gorilla/websocket"
@@ -20,13 +18,7 @@ type Session struct {
 }
 
 func (s *Session) NewSession() {
-	var addr = flag.String("addr", s.Host, "http service address")
-	flag.Parse()
-	log.SetFlags(0)
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-
-	u := url.URL{Scheme: "ws", Host: *addr, Path: fmt.Sprintf("/%s", s.Channel)}
+	u := url.URL{Scheme: "ws", Host: s.Host, Path: fmt.Sprintf("/%s", s.Channel)}
 	log.Printf("connecting to %s", u.String())
 	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
